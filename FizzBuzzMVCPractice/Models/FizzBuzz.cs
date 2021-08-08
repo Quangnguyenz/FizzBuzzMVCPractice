122b5776f7b1bcddb1bbb574dba4c154334ff87a
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2021-08-07 19:27:07 -0500

    Initial commit

---
+++ FizzBuzzMVCPractice/Models/FizzBuzz.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FizzBuzzMVCPractice.Models
+{
+    public class FizzBuzz
+    {
+        public int FizzValue;
+        public int BuzzValue;
+        public List<string> results = new();
+    }
+}
